d3cbadc54793f2a2a9daf1469f34b967974d0f56
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-05-12 17:07:07 -0400

    Initial commit, complete iteration 1 and 2

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,2 +1,25 @@
 class Classroom
+  attr_reader :subject, :capacity, :students
+
+  def initialize(subject, capacity, students=[])
+    @subject = subject
+    @capacity = capacity
+    @students = students
+  end
+
+  def add_student(student)
+    @student = student
+    students << student
+  end
+
+  def yell_at_students
+    students.map!(&:upcase)
+  end
+
 end
+
+classroom = Classroom.new('History', 4)
+classroom.add_student('Mike')
+classroom.add_student('Megan')
+classroom.add_student('Bob')
+classroom.yell_at_students
